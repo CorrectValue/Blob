466b3f65d7fec93ecdd53fc9c076100f7c77f3b6
Lexa <maythe4bewyou@gmail.com>
2020-11-09 19:48:11 +0400

    Day 3
    Added simple map generator with 3 different types of terrain. For now,
    different terrain tiles feature only different appearance, but no other
    properties.

--- Blob/Assets/Scripts/MapGenerator.cs
+++ Blob/Assets/Scripts/MapGenerator.cs
@@ -5,17 +5,56 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
 
-    //generates map using tiles
+    //This script is needed to perform tile-based map generation
+
+    public GameObject prefab;   //a tile
+    private GameObject player;  //player game object to create map around him
+    private GameObject parent;  //parent game object for tiles
+    public int renderDistance;  //a distance at which tiles emerge
+
+    private Vector3 pos;  //position to place tile
+    private Quaternion rot;  //rotation to place tile
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //find player game object
+        player = GameObject.FindWithTag("Player");
+        //find parent game object
+        parent = GameObject.Find("Map");
+
+        //create random map starting from the 0 point
+        for(int i = 0; i < renderDistance * renderDistance; i++)
+        {
+            //place some tile
+            var obj = Instantiate(prefab, GeneratePosition(i, renderDistance, prefab.GetComponent<TileController>().GetSize()), rot);
+            //set tile properties
+            var scr = obj.GetComponent<TileController>();
+            //set tile type
+            scr.SetType(Random.Range(0, 3)); //correct and set total amount of tile types when it will be ready
+            //set tag for object
+            obj.gameObject.tag = "Tile";
+            //put object into parent game object
+            obj.transform.parent = parent.transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //place new tiles as player walks further
+    }
+
+    Vector3 GeneratePosition(int index, int rDistance, Vector3 size)
+    {
+        //generates position for a tile
+        float newx, newy, newz;
+        newy = 0.0f;
+        //from index and rDistance we can get row and column to spawn a tile
+        int col = index % rDistance - rDistance / 2;
+        int row = index / rDistance - rDistance / 2;
+        newx = col * size.x;
+        newz = row * size.z;
+        return new Vector3(newx, newy, newz);
     }
 }
---
+++ Blob/Assets/Scripts/TileController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+
+    //This script is needed to store information about a single tile in order to place them using map generator
+    public int tileType; //a type of a tile: for now there will be grass, concrete and water tiles
+
+    public Texture grass;       //grass texture is 0
+    public Texture concrete;    //concrete texture is 1
+    public Texture water;       //water texture is 2
+
+    Renderer rnd;               //Renderer component
+
+    public Vector3 objectSize;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        //get Renderer
+        rnd = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeMaterial(int type)
+    {
+        //changes material of a tile when its type changed
+        //set texture
+        Debug.Log(rnd);
+        switch(type)
+        {
+            case 0:
+                rnd.material.SetTexture("_MainTex", grass);
+                break;
+            case 1:
+                rnd.material.SetTexture("_MainTex", concrete);
+                break;
+            case 2:
+                rnd.material.SetTexture("_MainTex", water);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void SetType(int type)
+    {
+        //sets type of a tile
+        tileType = type;
+        //change mat
+        ChangeMaterial(type);
+    }
+
+    public Vector3 GetSize()
+    {
+        //return size of an object
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        Bounds bounds = mesh.bounds;
+        return Vector3.Scale(transform.localScale, mesh.bounds.size); 
+    }
+}
